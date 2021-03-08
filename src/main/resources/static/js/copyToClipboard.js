89d75020c13dc345e54878619ce4d0f2f2611c2d
TomSuworof <rc1995king@gmail.com>
2021-03-08 18:12:11 +0300

    Fixed bug with copying link

--- src/main/resources/static/js/copyToClipboard.js
+++ src/main/resources/static/js/copyToClipboard.js
@@ -1,4 +1,4 @@
-function copyToClipboard() {
-    const link = document.location.host.toString() + document.getElementById("link").value.toString();
+function copyToClipboard(str) {
+    const link = document.location.host.toString() + str;
     navigator.clipboard.writeText(link).then(() => alert("Link copied to clipboard: " + link));
 }
