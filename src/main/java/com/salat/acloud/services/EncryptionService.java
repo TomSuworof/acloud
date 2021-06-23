3d98b5ff4417f879dcff5dba83b59f27171512c5
TomSuworof <rc1995king@gmail.com>
2021-06-23 12:03:47 +0300

    Cleanup

--- src/main/java/com/salat/acloud/services/EncryptionService.java
+++ src/main/java/com/salat/acloud/services/EncryptionService.java
@@ -1,39 +1,11 @@
 package com.salat.acloud.services;
 
-import java.io.*;
-import java.nio.file.Files;
-
 public class EncryptionService {
 
 //    public String publicKey;
 //    public String privateKey;
 
     private static final int shift = 42;
-
-//    public EncryptionService(String publicKey) {
-//        this.publicKey = publicKey;
-//        shift = Integer.parseInt(this.publicKey);
-//    }
-
-    public static void main(String[] args) {
-        try {
-            File file = new File("resume.pdf");
-            byte[] content = Files.readAllBytes(file.toPath());
-
-            content = encode(content);
-            content = decode(content);
-
-            File newFile = new File("resumeNew.pdf");
-
-            OutputStream writer = new FileOutputStream(newFile);
-            writer.write(content);
-
-            writer.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static byte[] encode(byte[] src) {
         // simple Caesar cipher
